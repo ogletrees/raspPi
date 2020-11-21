207f196607ce232fc874b2c2368221249056612f
ogletrees <ogletrees@users.noreply.github.com>
2020-11-21 13:30:03 -0500

    Create sensehat_logger.py

---
+++ sensehat_logger.py
@@ -0,0 +1,16 @@
+from sense_hat import SenseHat
+from datetime import datetime
+
+sense = SenseHat()
+
+def get_sense_data():
+	sense_data = []
+  sense_data.append(sense.get_temperature())
+  sense_data.append(sense.get_humidity())
+  sense_data.append(sense.get_pressure())
+  acc = sense.get_accelerometer_raw()
+  sense_data.append(acc["x"])
+  sense_data.append(acc["y"])
+  sense_data.append(acc["z"])
+  sense_data.append(datetime.now())
+  return sense_data
