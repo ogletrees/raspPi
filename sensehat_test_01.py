1082436682881926d484966325de1d7f90570b26
ogletrees <ogletrees@users.noreply.github.com>
2020-11-21 13:14:31 -0500

    Update sensehat_test_01.py

--- sensehat_test_01.py
+++ sensehat_test_01.py
@@ -1,4 +1,11 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 
-sense.show_message("Hello world")
+# sense.show_message("Hello world")
+
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+
+while True:
+  sense.show_message("Astro Pi is awesome!", text_colour=yellow, back_colour=blue, scroll_speed=0.05)
+
