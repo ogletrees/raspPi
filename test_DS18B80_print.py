1b787b627b09ec98871d57b61441e7e56fb12234
ogletrees <ogletrees@users.noreply.github.com>
2022-04-16 15:32:43 +0100

    Add files via upload

---
+++ test_DS18B80_print.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import os
+import re
+from csv import writer
+from datetime import datetime
+import time
+
+
+
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+
+def getTemps():
+  allTemps = list()
+  ow_basedir = "/sys/bus/w1/devices/"
+  ow_devices = os.listdir(ow_basedir)
+  ow_retemp = re.compile('t=(\d*)')
+  for thistring in ow_devices:
+    if(thistring.startswith("28")):
+      ow_path = os.path.join(ow_basedir, thistring, "w1_slave")
+      ow_devfile = open(ow_path, "r")
+      ow_devtext = ow_devfile.readlines()
+      ow_temp = ow_retemp.search(ow_devtext[1])
+      allTemps.append(ow_temp.group(1))
+  return(allTemps)
